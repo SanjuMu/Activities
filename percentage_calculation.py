1e9a1fb68847bd235c02bb3736ead1c78abbd149
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-19 17:09:29 -0500

    percentage_calculation

---
+++ percentage_calculation.py
@@ -0,0 +1,14 @@
+print("Enter the marks you got on the 4 Subjects:")
+subject_1 = int(input("Subject 1 - Math:"))
+subject_2= int(input("Subject 2 - English:"))
+subject_3=int(input("Subject 3 - Hindi:"))
+subject_4= int(input("Subject 4 - Science:"))
+
+sum = subject_1+subject_2+subject_3+subject_4
+
+percent = (sum/400)*100
+
+print (" Percentage mark=")
+print(percent)
+
+
